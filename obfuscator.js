bd1ba8952e429af9a268aa593ccbe861dcca6cdf
lenur <lenur.m@wecandevelopit.com>
2016-06-21 11:42:42 +0300

    update

--- obfuscator.js
+++ obfuscator.js
@@ -2,44 +2,9 @@
  * @param {Array} data – массив CSS классов
  */
 function obfuscator(array_elements) {
-/*
-    function countRepeatItems(arr) {
-        var result = [];
-        var current;
-        var isExist = false;
-        var data = null;
 
-        for (var i=0; i < arr.length; i++) {
-            current = arr[i];
-            data = {
-                name: current,
-                count: 1
-            };
-            if (result.length > 0) {
-                for (var j=0; j < result.length; j++) {
-                    if (result[j].name == current) {
-                        result[j].count++;
-                        isExist = true;
-                        break;
-                    }
-                }
-                if (!isExist) {
-                    result.push(data);
-                }
-            } else {
-                result.push(data);
-            }
-            isExist = false;
-        }
-
-        return result;
-    }
-	
-	var sortArr = countRepeatItems(array_elements).sort(function(a,b){
-		return b.count - a.count;
-    });
-	*/
 	var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	var objObfuscator = {};
 
 	function countRepeatItems(arr) {		
 		var data = {};
@@ -75,44 +40,29 @@ function obfuscator(array_elements) {
 		return arr;
 	}
 
+	function generate(i, limit) {
+		var div = Math.floor(i/limit);
+		var mod = i % limit;
+
+		return (div>0 ? generate(div - 1, limit) : []).concat(mod);
+	}
+
 	var objItems = countRepeatItems(array_elements);
 	var sortArr  = sortObject(objItems);
-	console.log('S', sortArr);
-	
-	var objObfuscator = {};
-    var result =[];
-	var str = '';
+
 	for (var i=0; i < sortArr.length; i++) {
 		var name = sortArr[i].name;
-		result = generate(i, 4);
-		console.log('result', result.join(''));
+		var result = generate(i, 4);
+		var str = '';
+
 		for(var j=0; j< result.length; j++) {
 			var index = result[j];
 			str = str + alphabet[index];
 		}
 
 		objObfuscator[name] = str;
-		result = [];
-		str = '';
     }
 
+	console.log('Sort', sortArr);
 	console.log('objObfuscator', objObfuscator);
-
-	function generate(i, limit) {
-		//if (i == 0) {
-		//	return;
-		//}
-
-		var div = Math.floor(i/limit);
-		var mod = i % limit;
-
-		return (div>0 ? generate(div - 1, limit) : []).concat(mod);
-		/*
-		var d = Math.floor(num / lim);
-		var m = num % lim;
-
-		return (d > 0 ? generate(d - 1, lim) : []).concat([m]);
-		*/
-
-	}
 };
